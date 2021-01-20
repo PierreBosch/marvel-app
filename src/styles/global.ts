c09f56a2b134eb7ca52c590ac9a90ef9e0f54c2c
Pierre Boschetto <pierre.mota@gam.com.br>
2021-01-20 17:44:33 -0300

    Global styles and struture created

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased;
+        color: #FFF;
+        background: #312E38;
+    }
+
+    body, input, button {
+        font-size: 16px;
+        font-family: 'Nunito', sans-serif;
+    }
+
+    strong, h1,h2,h3,h4,h5,h6 {
+        font-weight: 500;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`;
+
